421b1689a670088b5c3f86d7a836d25eed096ebd
Abhishek <abhishek.dahad@gmail.com>
2025-05-06 21:34:09 -0400

    Added sparsify() and alter_matrix_data_mem() functions.
    sparsify() responsible for creating actual recommendation matrix from pandas dataframe.
    alter_matrix_data_mem() responsible for modifying an individual value in the recommendation matrix held in memory.
    
    In future create a more efficient version that doesn't necessarily store entire recommendation matrix in memory at once.

--- algorithm/rec_algorithm.py
+++ algorithm/rec_algorithm.py
@@ -3,10 +3,14 @@ import numpy as np
 import pandas as pd
 
 
-matrix_data = None
+matrix_data_disk = None
+matrix_data_mem = None
+
+num_users = 0
+num_medium = 0
 
 """ 
-This function is responsible for loading training data from the disk. 
+load_data() is responsible for loading training data from the disk. 
 
 If loaded for the first time, then it will process the data to store in a 
 userID vs mediumID format.
@@ -14,7 +18,7 @@ If not loaded for the first time, then it will simply load the data from the
 disk.
 """
 def load_data(medium = ""):
-    global matrix_data
+    global matrix_data_disk
     # Handle error-checking here.
     medium_list = []
     medium_list.append("movie")
@@ -24,17 +28,19 @@ def load_data(medium = ""):
     if(medium not in medium_list):
         print(f"{medium} is not a valid medium to train on")
         return
-    
-    matrix_data = pd.read_csv(f"{medium}_ratings")
+    elif(not os.path.isfile(f"data/{medium}_ratings.csv")):
+        print(f"{medium} training data is missing")
+        return
+    matrix_data_disk = pd.read_csv(f"data/{medium}_ratings.csv")
     
 """ 
-This function is responsible for storing the dataframe into a .csv to be accessed
+store_data() is responsible for storing the dataframe into a .csv to be accessed
 at some other time.
-TODO: matrix_data can also contain predicted values for the user. Need to wipe those
+TODO: matrix_data_disk can also contain predicted values for the user. Need to wipe those
 before writing back.
 """
 def store_data(medium = ""):
-    global matrix_data
+    global matrix_data_disk
 
     # Handle error-checking here.
     medium_list = []
@@ -43,5 +49,62 @@ def store_data(medium = ""):
     medium_list.append("anime")
 
     #TODO: Need to handle bug where the medium that is edited can be stored as the csv for another medium
-    matrix_data.to_csv(f"{medium}_ratings")
+    matrix_data_disk.to_csv(f"data/{medium}_ratings")
     return
+
+"""
+sparsify() is responsible for converting the .csv file in the data into a userID-mediumID rating
+matrix.
+"""
+def sparsify(medium = ""):
+    
+    global num_users, num_medium, matrix_data_mem
+
+    #Need to increment by 1 because data is 1-indexed
+    num_users = max(matrix_data_disk.get("userId")) + 1
+    num_medium = max(matrix_data_disk.get(f"{medium}Id")) + 1
+    
+    # We are using -1 to represent a "no-rating"
+    matrix_data_mem = np.ones((num_users, num_medium)) * -1
+
+    #TODO: Vectorize this calculation here.
+    for row, element in matrix_data_disk.iterrows():
+        matrix_data_mem[int(element.loc["userId"])][int(element.loc[f"{medium}Id"])] = element.loc["rating"]
+    return
+
+
+
+"""
+add_user() is responsible for adding new users to the recommendation matrix.
+"""
+def add_user():
+    return
+
+
+""" 
+alter_matrix_data_mem() is responsible for indexing into the dataframe and then changing rating values
+given a specific user_id, for a specific medium_id, with a specific rating.
+"""
+def alter_matrix_data_mem(user_id, medium_id, rating):
+    
+    if(user_id < 0 or user_id > num_users):
+        print("userId is invalid")
+        return
+    elif(medium_id < 0 or medium_id > num_medium):
+        print("mediumId is invalid")
+        return
+    elif(rating < 0 or rating > 5):
+        print("rating is invalid")
+        return
+
+    matrix_data_mem[user_id][medium_id] = rating
+
+    return
+
+
+def main():
+    load_data("movie")
+    sparsify("movie")
+
+if __name__ == "__main__":
+    main()
